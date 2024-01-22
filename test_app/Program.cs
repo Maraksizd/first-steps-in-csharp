e41fa1f38539ee0122597391ea7bb9b09457aab2
Максим'як Данило <marakasizd@gmail.com>
2024-01-22 18:06:54 +0200

    Трикутники

--- test_app/Program.cs
+++ test_app/Program.cs
@@ -1,4 +1,4 @@
-﻿//Калькулятор
+﻿//Перевірка з трикутниками+вивід доаткової інформації про трикутник
 
 using System;
 
@@ -6,82 +6,71 @@ namespace test_app
 {
     internal class Program
     {
-        static void add(int value1, int value2)
-        {
-            Console.WriteLine(value1 + value2);
-        }
-
-        static void minus(int value1, int value2)
-        {
-            Console.Write(value1 - value2);
-        }
-
-        static void multip(int value1, int value2)
-        {
-            Console.WriteLine(value1 * value2);
-        }
-
-        static void dilenia(int value1, int value2)
-        {
-            double result = value1 / value2;
-            Console.WriteLine(result);
-        }
-
-        static int choise_metod()
-        {
-            int z;
-            Console.WriteLine("Виберіть дію:");
-            Console.WriteLine("1. Додавання");
-            Console.WriteLine("2. Віднімання");
-            Console.WriteLine("3. Множення");
-            Console.WriteLine("4. Ділення");
-
-            z = Int32.Parse(Console.ReadLine());
-            return z;
-        }
+        private const double pi = 3.14;
 
         static void Main(string[] args)
         {
-            int finish=1;
-            Console.WriteLine("Вас вітає наш примітивний калькулятор, розрочнімо роботу!");
 
-            while (finish==1)
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int a, b, c;
+            Console.WriteLine("Привіт, ця програма створена для перевірки трикутника на правильність. Щоб перевірити, чи трикутник правильний, введіть довжину сторін:");
+
+            Console.WriteLine("Введіть сторону a:");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть сторону b:");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть сторону c:");
+            c = Int32.Parse(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
             {
-                int a, b, znak;
+                Console.WriteLine("\nТакий трикутник існує!\n");
 
-                znak = choise_metod();
-
-                Console.WriteLine("Введіть число 1:");
-                a = Int32.Parse(Console.ReadLine());
-
-                Console.WriteLine("Введіть число 2:");
-                b = Int32.Parse(Console.ReadLine());
-
-                switch (znak)
-                {
-                    case 1:
-                        add(a, b);
-                        break;
-
-                    case 2:
-                        minus(a, b);
-                        break;
-
-                    case 3:
-                        multip(a, b);
-                        break;
-
-                    case 4:
-                        dilenia(a, b);
-                        break;
-                }
-
-                Console.WriteLine("Бажаєте продовжити?");
-                Console.WriteLine("1. Так");
-                Console.WriteLine("2. Ні");
-                finish = Int32.Parse(Console.ReadLine());
+                double S = triangle_area(a, b, c);
+                Console.WriteLine("Його площа буде становити: " + S);
+                find_angle(a, b, c);
             }
+            else
+            {
+                Console.WriteLine("\nТакого трикутника не існує");
+            }
+        }
 
+        static void find_angle(int a, int b, int c)
+        {
+            double s = triangle_area(a, b, c);
+
+            double Ha = 2 * s / a;
+            double Hb = 2 * s / b;
+            double Hc = 2 * s / c;
+
+            double A, B, C;
+
+            A = Math.Asin(Ha / c);
+            B = Math.Asin(Hb / a);
+            C = Math.Asin(Hc / b);
+
+            double angle_a = A * 180 / pi;
+            double angle_b = B * 180 / pi;
+            double angle_c = C * 180 / pi;
+
+            int angA = Convert.ToInt32(angle_a);
+            int angB= Convert.ToInt32(angle_b);
+            int angC= Convert.ToInt32(angle_c);
+
+            Console.WriteLine("Кут А: " + angA+" градусів");
+            Console.WriteLine("Кут В: " + angB + " градусів");
+            Console.WriteLine("Кут С: " + angC + " градусів");
+        }
+
+        static double triangle_area(int x, int y, int z)
+        {
+            int P = x + y + z;
+            double area = Math.Sqrt(P / 2 * (P / 2 - x) * (P / 2 - y) * (P / 2 - z));
+            return area;
         }
     }
 }
